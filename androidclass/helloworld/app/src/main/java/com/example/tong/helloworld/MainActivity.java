64b324b19bf496856e691772b29f541b045efe92
Sean <tyx513@163.com>
2019-03-12 11:00:58 +0800

    activity lifecircle

--- androidclass/helloworld/app/src/main/java/com/example/tong/helloworld/MainActivity.java
+++ androidclass/helloworld/app/src/main/java/com/example/tong/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tong.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity","onCreate");
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity","onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity","onResume");
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity","onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity","onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MainActivity","onRestart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity","onDestroy");
+
+    }
+
 }
