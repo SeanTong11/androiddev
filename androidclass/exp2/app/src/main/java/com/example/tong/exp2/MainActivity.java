6c59566bb02a72951b1b401e692f267c35986771
Sean <tyx513@163.com>
2019-03-12 20:06:05 +0800

    exp2 complete

--- androidclass/exp2/app/src/main/java/com/example/tong/exp2/MainActivity.java
+++ androidclass/exp2/app/src/main/java/com/example/tong/exp2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tong.exp2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void jump1 (View view){
+        //Do something in response to button
+        Intent intent = new Intent(this, linearActivity.class);
+        startActivity(intent);
+    }
+
+    public void jump2 (View view){
+        //Do something in response to button
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
+    }
+
+    public void jump3 (View view){
+        //Do something in response to button
+        Intent intent = new Intent(this, TableActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ androidclass/exp2/app/src/main/java/com/example/tong/exp2/linear.java
@@ -0,0 +1,4 @@
+package com.example.tong.exp2;
+
+public class linear {
+}
