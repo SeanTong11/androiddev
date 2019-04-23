0f6738b0d82eb5cdaf04f08b9f8a2aa5b85ebb37
Sean <tyx513@163.com>
2019-04-23 10:49:34 +0800

    exp4 1

---
+++ androidclass/exp4/app/src/main/java/com/example/tong/exp4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tong.exp4;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void openActivity(View view) {
+        Intent intent=new Intent(this,SettingsActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
